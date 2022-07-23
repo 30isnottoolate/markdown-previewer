241900ea77dc74e661c9d8d4ca9003cc8c2497c4
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-23 17:13:52 +0200

    Render MarkdownPreviewer from index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './script.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MarkdownPreviewer from './MarkdownPreviewer';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <MarkdownPreviewer />
+  </React.StrictMode>
+);
